f45b8fadaa032ccb559db5cc64fb913d503f93b7
righi.andrea <righi.andrea@c816962c-453b-0410-bd98-89700c55ec60>
2007-09-29 12:23:22 +0000

    initial release
    git-svn-id: http://googlecalendarsync.googlecode.com/svn/trunk@2 c816962c-453b-0410-bd98-89700c55ec60

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+from distutils.core import setup
+from googlecalsync import __version__
+
+setup(name='googlecalsync',
+      version=__version__,
+      description="Google Calendar Synchronizer",
+      author="Andrea Righi",
+      author_email="righiandr@users.sf.net",
+      scripts=['googlecalsync.py'],
+      packages=None
+      )
+
